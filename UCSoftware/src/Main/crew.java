26d74108037dee825082253155a8a3ae5160986c
ewrt <ewrt101@gmail.com>
2019-05-01 09:52:41 +1200

    some code

---
+++ UCSoftware/src/Main/crew.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class crew {
+
+	private String name;
+	
+	
+	public void set_name(String Name) {
+		name = Name;
+	}
+		
+	
+}
---
+++ UCSoftware/src/Main/game_environment.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * @author daniel
+ *
+ */
+public class game_environment {
+
+	
+	private int days = 0;
+	crew Crew = new crew();
+	
+	
+	// day controlers
+	public void set_days(int day) {
+		days = day;
+	}
+	public void add_day () {
+		days += 1;
+	}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
