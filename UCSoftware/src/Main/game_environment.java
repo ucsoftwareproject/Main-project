c2a2e9e7c2834896f01af9644628341175d3156f
ewrt <ewrt101@gmail.com>
2019-05-10 12:42:48 +1200

    added some items and some debug code

--- UCSoftware/src/Main/game_environment.java
+++ UCSoftware/src/Main/game_environment.java
@@ -3,12 +3,17 @@
  */
 package Main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author daniel
  *
  */
 public class game_environment {
 
+	
+	protected List<item> master_items = new ArrayList<>();
 	
 	private int days = 0;
 	crew Crew = new crew();
@@ -26,12 +31,47 @@ public class game_environment {
 	}
 	
 	
+	
+	//builds Master_ items list
+	private void build_items() {
+		
+		// food
+		master_items.add( new item("cake", 0, 40f, 0, 100f));
+		master_items.add( new item("rations", 1, 10f, 0, 5f));
+		master_items.add( new item("soda", 2, 1f, 0, 2f));
+		master_items.add( new item("apple", 3, 25f, 0, 50f));
+		
+		//meds
+		master_items.add( new item("Med Kit", 4, 40f, 1, 100f));
+		master_items.add( new item("band aid", 5, 5f, 1, 10f));
+		master_items.add( new item("pain killer", 6, 10f, 1, 15f));
+		master_items.add( new item("bandage", 7, 25f, 1, 50f));
+		
+		
+		
+		// for testing
+		System.out.println("Master items:");
+		for (item s: master_items) {
+			System.out.println("\t* " + s.get_name());
+		}
+		System.out.println("\n");
+	}
+	
+	//main setup controller
+	public game_environment() {
+		build_items();
+	}
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		game_environment engine = new game_environment();
+		
+		
 	}
 
 }
--- UCSoftware/src/Main/item.java
+++ UCSoftware/src/Main/item.java
@@ -8,6 +8,22 @@ public class item {
 	private int type;
 	private float improvement;
 	
+	
+	
+	/**  
+	 * Item Types
+	 * Type 0 = food
+	 * Type 1 = Meds
+	 * Type 2 = both
+	 * */
+	public item(String Name, int Id, float Cost, int Type, float Improvement) {
+		name = Name;
+		id = Id;
+		cost = Cost;
+		type = Type;
+		improvement = Improvement;
+	}
+	
 	//gets the cost of an item
 	public float get_cost() {
 		return cost;
@@ -16,5 +32,15 @@ public class item {
 	//gets the type of item
 	public float get_type() {
 		return type;
+	}
+	
+	//gets the type of item
+	public String get_name() {
+		return name;
+	}
+	
+	//gets the type of item
+	public float get_improvement() {
+		return improvement;
 	}
 }
