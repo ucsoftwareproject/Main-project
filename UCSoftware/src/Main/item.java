6e363c76355e62a14e74d8a13eda128a9bb9d660
ewrt101 <ewrt101@gmail.com>
2019-05-23 14:53:33 +1200

    added item java docs

--- UCSoftware/src/Main/item.java
+++ UCSoftware/src/Main/item.java
@@ -1,21 +1,45 @@
 package Main;
 
-public class item {
+/**
+ * @author      Daniel McInnes dmc245@uclive.ac.nz , David
+ * @version     1.0
+ * @since       1.0
+ */
 
+public class item {
+	/**
+	 * Name of item
+	 */
 	private String name;
+	/**
+	 * id of item
+	 */
 	public int id;
+	/**
+	 * cost of item
+	 */
 	private float cost;
-	private int type;
-	private float improvement;
-	
-	
-	
-	/**  
+	/**
+	 * type of item
+	 * <p>
 	 * Item Types
 	 * Type 0 = food
 	 * Type 1 = Meds
 	 * Type 2 = both
-	 * */
+	 */
+	private int type;
+	/**
+	 * improvement amount
+	 */
+	private float improvement;
+	/**
+	 * main item builder
+	 * @param Name name of item
+	 * @param Id Id of item
+	 * @param Cost cost of item
+	 * @param Type Type of item 0,1,2
+	 * @param Improvement Improvement amount
+	 */
 	public item(String Name, int Id, float Cost, int Type, float Improvement) {
 		name = Name;
 		id = Id;
@@ -24,22 +48,34 @@ public class item {
 		improvement = Improvement;
 	}
 	
-	//gets the cost of an item
+	/**
+	 * gets item cost
+	 * @return cost of item
+	 */
 	public float getCost() {
 		return cost;
 	}
 	
-	//gets the type of item
+	/**
+	 * gets item type
+	 * @return item type
+	 */
 	public float getType() {
 		return type;
 	}
 	
-	//gets the name of item
+	/**
+	 * gets item name
+	 * @return name of item
+	 */
 	public String getName() {
 		return name;
 	}
 	
-	//gets the quality of item
+	/**
+	 * gets improvement amount
+	 * @return improvement amount
+	 */
 	public float getImprovement() {
 		return improvement;
 	}
